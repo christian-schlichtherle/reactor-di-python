19c05fcd185e0fb3f002785986e2d80413782dc0
Christian Schlichtherle <christian@schlichtherle.de>
2025-07-15 13:45:02 +0200

    Rename example files to singular form and improve class naming
    - Rename caching_strategies.py to caching_strategy.py
    - Rename custom_prefixes.py to custom_prefix.py
    - Update class names: DevModule→FactoryModule, ProdModule→SingletonModule
    - Improve test function naming for clarity

--- examples/caching_strategy.py
+++ examples/caching_strategy.py
@@ -21,7 +21,7 @@ class MyService:
 
 # No caching - components created fresh each time
 @module(CachingStrategy.DISABLED)
-class DevModule:
+class FactoryModule:
     """Development module with no caching."""
 
     service: MyService
@@ -29,7 +29,7 @@ class DevModule:
 
 # Cached components - same instance returned (not thread-safe)
 @module(CachingStrategy.NOT_THREAD_SAFE)
-class ProdModule:
+class SingletonModule:
     """Production module with caching enabled."""
 
     service: MyService
@@ -37,12 +37,12 @@ class ProdModule:
 
 def test_disabled_caching():
     """Test that DISABLED strategy creates fresh instances each time."""
-    dev_module = DevModule()
+    factory_module = FactoryModule()
 
     # Get service multiple times
-    service1 = dev_module.service
-    service2 = dev_module.service
-    service3 = dev_module.service
+    service1 = factory_module.service
+    service2 = factory_module.service
+    service3 = factory_module.service
 
     # Each call should return a different instance
     assert service1 is not service2
@@ -56,12 +56,12 @@ def test_disabled_caching():
 
 def test_not_thread_safe_caching():
     """Test that NOT_THREAD_SAFE strategy caches instances."""
-    prod_module = ProdModule()
+    singleton_module = SingletonModule()
 
     # Get service multiple times
-    service1 = prod_module.service
-    service2 = prod_module.service
-    service3 = prod_module.service
+    service1 = singleton_module.service
+    service2 = singleton_module.service
+    service3 = singleton_module.service
 
     # All calls should return the same instance
     assert service1 is service2
@@ -75,11 +75,11 @@ def test_not_thread_safe_caching():
 
 def test_different_modules_have_different_instances():
     """Test that different module instances have different cached services."""
-    prod_module1 = ProdModule()
-    prod_module2 = ProdModule()
+    singleton_module1 = SingletonModule()
+    singleton_module2 = SingletonModule()
 
-    service1 = prod_module1.service
-    service2 = prod_module2.service
+    service1 = singleton_module1.service
+    service2 = singleton_module2.service
 
     # Different module instances should have different service instances
     assert service1 is not service2
@@ -88,15 +88,15 @@ def test_different_modules_have_different_instances():
 
 def test_caching_strategy_comparison():
     """Test that different caching strategies behave differently."""
-    dev_module = DevModule()
-    prod_module = ProdModule()
+    factory_module = FactoryModule()
+    singleton_module = SingletonModule()
 
     # Test dev module (no caching)
-    dev_service1 = dev_module.service
-    dev_service2 = dev_module.service
+    dev_service1 = factory_module.service
+    dev_service2 = factory_module.service
     assert dev_service1 is not dev_service2  # Different instances
 
     # Test prod module (with caching)
-    prod_service1 = prod_module.service
-    prod_service2 = prod_module.service
+    prod_service1 = singleton_module.service
+    prod_service2 = singleton_module.service
     assert prod_service1 is prod_service2  # Same instance
